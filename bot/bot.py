5e0149635e3e40d287adff4c7be2ae03de075409
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-06-11 15:08:17 +0530

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,17 @@
+
+import os
+import logging
+import ffmpeg
+import asyncio
+import json
+import requests
+from pyrogram import Client, filters, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from vars import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "Shazam",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
